71e41cc3353b7a0572a4143bb7ba67d3e7d9384c
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-06-28 11:59:57 -0300

    .

--- ProyectoFinal/ProyectoFinal2/public/index.js
+++ ProyectoFinal/ProyectoFinal2/public/index.js
@@ -41,10 +41,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Funcion para asignar nombre a los jugadores en caso de que sea vacio
   function asignarNombre(jugadores) {
-    if (jugadores[0].nombre === "" || jugadores[0].nombre === null) {
+    if (jugadores[0].nombre === "" || jugadores[0].nombre === null || jugadores[0].nombre === undefined) {
       jugadores[0].nombre = "Jugador 1";
     }
-    if (jugadores[1].nombre === "" || jugadores[1].nombre === null) {
+    if (jugadores[1].nombre === "" || jugadores[1].nombre === null || jugadores[1].nombre === undefined) {
       jugadores[1].nombre = "Jugador 2";
     }
   }
