dd9806ad3ea191c3f9dd2a9775d886f981d4f831
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-06-28 11:09:57 -0300

    cambios en quien gana la partida

--- ProyectoFinal/ProyectoFinal2/public/index.js
+++ ProyectoFinal/ProyectoFinal2/public/index.js
@@ -152,11 +152,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (turnoGanador == 1) {
       tituloGanador.style.backgroundColor = coloresJugadores[0];
-      tituloGanador.innerText = `Jugador ${nombreGanador} ha ganado el juego!`;
     } else {
       tituloGanador.style.backgroundColor = coloresJugadores[1];
-      tituloGanador.innerText = `Jugador ${nombreGanador} ha ganado el juego!`;
     }
+
+    tituloGanador.innerText = `${nombreGanador} ha ganado el juego!`;
   });
 
   btnDado.addEventListener("click", () => {
