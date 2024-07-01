96286941b2b9260b6b8009ed3ea40412d5d66f56
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-07-01 11:13:52 -0300

    comentario

--- ProyectoFinal/ProyectoFinal2/public/index.js
+++ ProyectoFinal/ProyectoFinal2/public/index.js
@@ -263,6 +263,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function mezclarRespuestas(array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
+      // Intercambiar elementos
       [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
